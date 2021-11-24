42ccd339ee3604b70d0db1161114fa2aad9b2c25
Anaena <carrie@mail.ru>
2021-11-24 14:41:57 +0300

    Добавляет блок accordion

--- source/js/main.js
+++ source/js/main.js
@@ -25,3 +25,25 @@ menuButtonElement.addEventListener('click', () => {
     body.classList.remove('page__body--locked');
   }
 });
+
+// Accordion
+
+const accordion = document.querySelector('.accordion');
+const accordionTitle = document.querySelectorAll('.accordion__title');
+const accordionItem = document.querySelectorAll('.accordion__item');
+
+accordion.classList.remove('accordion--nojs');
+
+accordionTitle.forEach((item) =>
+  item.addEventListener('click', () => {
+    const parent = item.parentNode;
+
+    if (parent.classList.contains('accordion__item--active')) {
+      parent.classList.remove('accordion__item--active');
+    } else {
+      accordionItem.forEach((child) =>
+        child.classList.remove('accordion__item--active'));
+      parent.classList.toggle('accordion__item--active');
+    }
+  })
+);
